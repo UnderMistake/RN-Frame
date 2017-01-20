cda399385943039c3cf233a9b55897f1139c56d3
wzx <455791779@qq.com>
2017-01-21 05:31:49 +0800

    add homeContainer

---
+++ Rn_Frame/App/containers/homeContainer.js
@@ -0,0 +1,32 @@
+/**
+ * Created by wzx on 17/1/21.
+ */
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Dimensions
+} from 'react-native';
+
+import {getNavigator} from '../route';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
+
+class HomeContainer extends Component {
+
+    render(){
+        return(
+            <View style={styles.container}>
+                <Text>123</Text>
+            </View>
+        );
+    }
+}
+
+export default HomeContainer;
